5476367ea763b5aa5df452a40aa60c4e7f0bb6d9
Jeff Hacker <reachjkh@yahoo.com>
2015-07-27 12:49:23 -0400

    models built.  Data not yet migrated

--- fivex_charts/fxcm/models.py
+++ fivex_charts/fxcm/models.py
@@ -1,5 +1,43 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
 
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, null=True)
+    member = models.BooleanField(default=False)
+    administrator = models.BooleanField(default=False)
+    email = models.EmailField()
+
+    def __str__(self):
+        return 'username: {}'.format(self.user)
+
+
+class ClosedTrades(models.Model):
+    user = models.ForeignKey(UserProfile)
+    ticket = models.IntegerField()
+    symbol = models.CharField(max_length=7)
+    volume = models.DecimalField(max_digits=None, decimal_places=2)
+    opendatetime = models.DateTimeField()
+    soldprice = models.DecimalField(max_digits=None, decimal_places=5)
+    closedatetime = models.DateTimeField()
+    boughtprice = models.DecimalField(max_digits=None, decimal_places=5)
+    grossprofitloss = models.DecimalField(max_digits=None, decimal_places=2)
+    comm = models.DecimalField(max_digits=None, decimal_places=2)
+    dividends = models.DecimalField(max_digits=None, decimal_places=2)
+    rollover = models.DecimalField(max_digits=None, decimal_places=2)
+    adj = models.DecimalField(max_digits=None, decimal_places=2)
+    netprofitloss = models.DecimalField(max_digits=None, decimal_places=2)
+    account = models.IntegerField()
+    journal = models.TextField()
+
+
+class AccountActivity(models.Model):
+    timeposted = models.DateTimeField()
+    code = models.CharField(max_length=10)
+    description = models.TextField(max_length=100)
+    account = models.IntegerField()
+    ticket = models.IntegerField()
+    amount = models.DecimalField(max_digits=None, decimal_places=2)
+    balance = models.DecimalField(max_digits=None, decimal_places=2)
