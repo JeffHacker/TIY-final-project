971bd4972c4cdef6c41a30b051979e6d1639cc25
Jeff Hacker <reachjkh@yahoo.com>
2015-07-27 20:10:26 -0400

    models revised-ready for migration

--- fivex_charts/fxcm/models.py
+++ fivex_charts/fxcm/models.py
@@ -18,21 +18,24 @@ class ClosedTrades(models.Model):
     user = models.ForeignKey(UserProfile)
     ticket = models.IntegerField()
     symbol = models.CharField(max_length=7)
-    volume = models.DecimalField(max_digits=None, decimal_places=2)
+    volume = models.IntegerField()
     opendatetime = models.DateTimeField()
-    soldprice = models.DecimalField(max_digits=None, decimal_places=5)
     closedatetime = models.DateTimeField()
+    soldprice = models.DecimalField(max_digits=None, decimal_places=5)
     boughtprice = models.DecimalField(max_digits=None, decimal_places=5)
+    direction = models.CharField(max_length=5)
     grossprofitloss = models.DecimalField(max_digits=None, decimal_places=2)
     comm = models.DecimalField(max_digits=None, decimal_places=2)
     dividends = models.DecimalField(max_digits=None, decimal_places=2)
     rollover = models.DecimalField(max_digits=None, decimal_places=2)
     adj = models.DecimalField(max_digits=None, decimal_places=2)
     netprofitloss = models.DecimalField(max_digits=None, decimal_places=2)
-    account = models.IntegerField()
+    buycondition = models.CharField(max_length=3)
+    sellcondition = models.CharField(max_length=3)
+    createdbyaccount = models.IntegerField()
     journal = models.TextField()
 
-
+'''
 class AccountActivity(models.Model):
     timeposted = models.DateTimeField()
     code = models.CharField(max_length=10)
@@ -41,3 +44,4 @@ class AccountActivity(models.Model):
     ticket = models.IntegerField()
     amount = models.DecimalField(max_digits=None, decimal_places=2)
     balance = models.DecimalField(max_digits=None, decimal_places=2)
+'''
