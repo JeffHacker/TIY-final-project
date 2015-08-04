83f7775d493ac9175e13a9de0e7becc132a467d8
Jeff Hacker <reachjkh@yahoo.com>
2015-08-04 13:44:00 -0400

    login logout and user create

--- fivex_charts/fivex_charts/urls.py
+++ fivex_charts/fivex_charts/urls.py
@@ -13,16 +13,23 @@ Including another URLconf
     1. Add an import:  from blog import urls as blog_urls
     2. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
 """
+from django.contrib.auth.forms import UserCreationForm, PasswordChangeForm
+from django.views.generic.edit import  CreateView
 from django.conf.urls import include, url
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from fxcm.views import upload_data, hello, chart_list_view, matplot_lib
+from fxcm.views import upload_data, hello, matplot_lib, logout_view
 
 urlpatterns = [
+    url(r'^$', upload_data, name='upload_data'),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', upload_data, name='uploaod_data'),
+    url(r'^accounts/login/$', 'django.contrib.auth.views.login', name='login'),
+#    url(r'^logout/$','django.contrib.auth.views.logout', name='logout'),
+    url(r'^accounts/logout/$', logout_view, name='logout'),
     url(r'^hello/', hello, name='hello'),
     url(r'^upload_data/', upload_data, name='upload_data'),
     url(r'^charts/', matplot_lib, name='charts'),
+    url(r'^register/', CreateView.as_view(template_name='register.html', form_class=UserCreationForm, success_url='/')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
--- fivex_charts/fxcm/views.py
+++ fivex_charts/fxcm/views.py
@@ -1,16 +1,20 @@
 import numpy as np
 import pandas as pd
-from django.shortcuts import render_to_response
-from django.template import RequestContext
-from .models import UploadedData, ClosedTrade
-from .forms import UploadFileForm
 from .converter import scatter_to_base64
-
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout
+from django.shortcuts import render_to_response, redirect
+from django.template import RequestContext
+from .forms import UploadFileForm
+from .models import UploadedData, ClosedTrade
 
 # Create your views here.
 
+def logout_view(request):
+    logout(request)
+    return redirect('login')
 
-def matplot_lib(request):
+def matplot_lib(request):  # this creates the charts using converter.py
     data = UploadedData.objects.get(id=39)
     print(data.data)
     df = pd.read_csv(data.data)
@@ -28,21 +32,21 @@ def matplot_lib(request):
                                               , "graph_five": graph_five, "graph_six": graph_six
                                               , "graph_seven": graph_seven})
 
-
+@login_required
 def upload_data(request):
     if request.method == 'POST':
         form = UploadFileForm(request.POST, request.FILES)
         if form.is_valid():
             trade_list = list(request.FILES['data'])
             newdoc = UploadedData(data = request.FILES['data']) #data is the FileField in my UploadedData model
-            newdoc.save()
-            print(newdoc)
-            #how to read data here in the view
+            newdoc.save()  # save file to FileField
+            #print(newdoc)
+            #reading in the data fields
             for trade in trade_list[1:]: # or data?
-                cleantrade = trade.decode("utf-8") #Bekk
-                print("CLEANTRADE", cleantrade)
+                cleantrade = trade.decode("utf-8")  # Bekk
+                #print("CLEANTRADE", cleantrade)
                 trade_info = cleantrade.replace("\n", "").split(',')
-                print(trade_info)
+                #print(trade_info)
                 trade_info[0] = int(trade_info[0])
                 trade_info[2] = int(trade_info[2])
                 trade_info[5] = float(trade_info[5])
@@ -54,6 +58,7 @@ def upload_data(request):
                 trade_info[12] = float(trade_info[12])
                 trade_info[13] = float(trade_info[13])
                 trade_info[16] = int(trade_info[16])
+                # insert below - user=request.user
                 ClosedTrade.objects.create(data=newdoc, ticket=trade_info[0], symbol=trade_info[1], volume=trade_info[2],
                                             opendatetime=trade_info[3], closedatetime=trade_info[4], soldprice=trade_info[5],
                                             boughtprice=trade_info[6], direction=trade_info[7],
@@ -61,21 +66,16 @@ def upload_data(request):
                                             rollover=trade_info[11], adj=trade_info[12], netprofitloss=trade_info[13],
                                             buycondition=trade_info[14], sellcondition=trade_info[15],
                                             createdbyaccount=trade_info[16])
-
-
     form = UploadFileForm() # A empty, unbound form  #UploadFileForm is equal to DocumentForm in the tutorial
-
     # Load documents for the list page
     documents = UploadedData.objects.all()
-
-
     # Render list page with the documents and the form
     return render_to_response('upload_data.html',
                               {'documents': documents, 'form': form}, context_instance=RequestContext(request))
 
 
-def chart_list_view(request):  # I added this to test
-    return render_to_response("charts.html", context_instance=RequestContext(request))
+#def chart_list_view(request):  # I added this to test
+#    return render_to_response("charts.html", context_instance=RequestContext(request))
 
 
 def hello(request):
