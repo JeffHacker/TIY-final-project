bb21616473f824077859c7ca53399f40ecbc804e
Jeff Hacker <reachjkh@yahoo.com>
2015-08-11 11:33:07 -0400

    chart color for neg/pos nums

--- fivex_charts/fxcm/converter.py
+++ fivex_charts/fxcm/converter.py
@@ -15,47 +15,61 @@ def create_session_range(hour):
     else:
         return "Asia"
 
-
+import pandas as pd
 def scatter_to_base64(df, action):
-    plt.figure(figsize=(6, 6))
+    plt.figure(figsize=(6, 9))
     if action == "ave_pl_by_symbol":
         plt.title("Average Profit/Loss by Currency Pair", fontsize=20)
-        image = df.groupby(['symbol']).grossprofitloss.mean().plot(kind="bar")#,
-                                                                   #color=df.positive.map({True: 'r', False: 'g'}))
-        # plt.ylabel("Difference between Heads & Tails")
-        # plt.xlabel("2**x flips")
-
+        mean_df = pd.DataFrame(df.groupby(['symbol']).grossprofitloss.mean())
+        mean_df['positive'] = mean_df['grossprofitloss'] > 0
+        mean_df['grossprofitloss'].plot(kind="bar", color=mean_df.positive.map({True: 'g', False: 'r'}))
     elif action == "ave_pl_by_wkday":
-        df['dayofweek'] = pd.DatetimeIndex(df['opendatetime']).dayofweek
         plt.title("Average Profit/Loss by Day of the Week", fontsize=18)
-        image = df.groupby(df['dayofweek']).grossprofitloss.mean().plot(kind="bar", color='red')
+        df['dayofweek'] = pd.DatetimeIndex(df['opendatetime']).dayofweek
+        mean_df = pd.DataFrame(df.groupby(['dayofweek']).grossprofitloss.mean())
+        mean_df['positive'] = mean_df['grossprofitloss'] > 0
+        mean_df['grossprofitloss'].plot(kind="bar", color=mean_df.positive.map({True: 'g', False: 'r'}))
 
     elif action == "ave_pl_by_month":
         df['month'] = pd.DatetimeIndex(df['opendatetime']).month
         plt.title("Average Profit/Loss by Month of the Year", fontsize=18)
-        image = df.groupby(df['month']).grossprofitloss.mean().plot(kind="bar")
+        mean_df = pd.DataFrame(df.groupby(['month']).grossprofitloss.mean())
+        mean_df['positive'] = mean_df['grossprofitloss'] > 0
+        mean_df['grossprofitloss'].plot(kind="bar", color=mean_df.positive.map({True: 'g', False: 'r'}))
 
     elif action == "ave_pl_by_year":
         df['year'] = pd.DatetimeIndex(df['opendatetime']).year
         plt.title("Average Profit/Loss by Year", fontsize=18)
-        image = df.groupby(df['year']).grossprofitloss.mean().plot(kind="bar")
+        mean_df = pd.DataFrame(df.groupby(['year']).grossprofitloss.mean())
+        mean_df['positive'] = mean_df['grossprofitloss'] > 0
+        mean_df['grossprofitloss'].plot(kind="bar", color=mean_df.positive.map({True: 'g', False: 'r'}))
 
     elif action == "ave_pl_by_direction":
         plt.title("Average Profit/Loss by Direction of Trade", fontsize=18)
-        image = df.groupby(['direction']).grossprofitloss.mean().plot(kind="bar")
+        mean_df = pd.DataFrame(df.groupby(['direction']).grossprofitloss.mean())
+        mean_df['positive'] = mean_df['grossprofitloss'] > 0
+        mean_df['grossprofitloss'].plot(kind="bar", color=mean_df.positive.map({True: 'g', False: 'r'}))
 
     elif action == "ave_pl_by_session":
         df['hour'] = pd.DatetimeIndex(df['opendatetime']).hour
         df["session"] = df["hour"].map(create_session_range)
         plt.title("Average Profit/Loss by Trading Session", fontsize=18)
-        image = df.groupby(df['session']).grossprofitloss.mean().plot(kind="bar")
+        mean_df = pd.DataFrame(df.groupby(['session']).grossprofitloss.mean())
+        mean_df['positive'] = mean_df['grossprofitloss'] > 0
+        mean_df['grossprofitloss'].plot(kind="bar", color=mean_df.positive.map({True: 'g', False: 'r'}))
+
 
     elif action == "ave_pl_by_dir_session":
+        plt.title("Average Profit/Loss by Trade Direction per Session", fontsize=14)
         df['hour'] = pd.DatetimeIndex(df['opendatetime']).hour
         df["session"] = df["hour"].map(create_session_range)
-        pivoted_df = pd.pivot_table(df, index=["session", "direction"], values=["grossprofitloss"])
-        plt.title("Average Profit/Loss by Trade Direction per Session", fontsize=16)
-        image = pivoted_df.plot(kind="bar")
+        mean_df = pd.pivot_table(df, index=["session", "direction"], values=["grossprofitloss"])
+        mean_df['positive'] = mean_df['grossprofitloss'] > 0
+        mean_df['grossprofitloss'].plot(kind="bar", color=mean_df.positive.map({True: 'g', False: 'r'}))
+
+
+#        pivoted_df.plot(kind="bar")
+#        df.groupby(['direction']).gross_pl.mean().plot(kind="barh")
 
     #########################
     image_file = io.BytesIO()
