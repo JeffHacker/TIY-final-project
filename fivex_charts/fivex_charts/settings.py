cefefed66f26c0a48f07191b6b0e9913a46de862
Jeff Hacker <reachjkh@yahoo.com>
2015-09-06 13:33:43 -0400

    sqlite for aws demo

--- fivex_charts/fivex_charts/settings.py
+++ fivex_charts/fivex_charts/settings.py
@@ -75,11 +75,18 @@ WSGI_APPLICATION = 'fivex_charts.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'fxcmdata',
         'USER': 'forexuser',
+'''
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+
     }
 }
 
